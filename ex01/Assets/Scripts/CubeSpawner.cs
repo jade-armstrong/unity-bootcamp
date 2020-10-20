35193512a18edd2bd4288fa1d1ff1552e58d597a
jarmstro-wtc <jarmstro@student.wethinkcode.co.za>
2020-10-20 11:41:31 +0200

    Submitting D00 ex000 and ex01

---
+++ ex01/Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    public List<GameObject> cubeList = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", 1f, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Spawn()
+    {
+        int randomIndex = Random.Range(0, 3);
+        GameObject cube = GameObject.Instantiate(cubeList[randomIndex]);
+        Rigidbody2D cubeRb2d = cube.GetComponent<Rigidbody2D>();
+        cubeRb2d.gravityScale = Random.Range(0.5f, 1.2f);
+    }
+}
