6946aebf9a8e885706830d3ac615009584fc2a8e
jarmstro-wtc <jarmstro@student.wethinkcode.co.za>
2020-10-24 13:50:42 +0200

    Submitting d02/ex02

---
+++ d02/Assets/Scripts/UnitSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSpawner : MonoBehaviour
+{
+    public GameObject unitHandler;
+    UnitHandler handlerScript;
+    public GameObject human;
+    public GameObject orc;
+    GameObject instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        handlerScript = unitHandler.GetComponent<UnitHandler>();
+        InvokeRepeating("SpawnUnits", 2f, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnUnits()
+    {
+        Instantiate(orc);
+        instance = Instantiate(human);
+        handlerScript.footies.Add(instance);
+    }
+}
