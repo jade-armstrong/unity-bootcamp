419c1bee445d8ced569056f8bc3ac5f76abf84b5
jarmstro-wtc <jarmstro@student.wethinkcode.co.za>
2020-11-02 12:18:58 +0200

    Reorganizing folders

---
+++ d00/ex03/Assets/Scripts/Pipe.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    public float speed = 1f;
+    float pipeY = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position -= new Vector3(speed,0,0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "PipeRespawner")
+        {
+            pipeY = -1.26f + Random.Range(0, 4.86f);
+            gameObject.transform.position = new Vector3(11.6f, pipeY, -1f);
+        }
+    }
+}
